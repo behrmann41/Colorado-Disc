eacc2677700a36e7b722ec22bbf9b41084334fcf
Ian Behrmann <ianjbehrmann@gmail.com>
2015-09-19 16:09:53 -0600

    course search with results on page

--- routes/profile.js
+++ routes/profile.js
@@ -13,21 +13,11 @@ router.post('/', function(req, res, next) {
   //var search = req.body.coursesearch;
   var reg = new RegExp(req.body.coursesearch, "i")
   discCourses.find({name: reg }, function(err, data) {
-      console.log(data)
+    res.render('courses/profile', { updatePage: true,
+                                    courseSearch: data
+                                  })
   })
 })
-
-// function pagelist(items) {
-//   var result = "<html><body><ul>";
-//   items.forEach(function(item) {
-//     var itemstring = "<li>" + item._id + "<ul><li>" + item.textScore +
-//       "</li><li>" + item.created + "</li><li>" + item.document +
-//       "</li></ul></li>";
-//     result = result + itemstring;
-//   });
-//   result = result + "</ul></body></html>";
-//   return result;
-// }
 
 module.exports = router;
 
