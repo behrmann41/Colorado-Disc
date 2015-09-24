56c7b3f3e788683495e2f5aa6c5ad2a94e87af21
Ian Behrmann <ianjbehrmann@gmail.com>
2015-09-24 11:49:01 -0600

    added images with links on homepage

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -18,15 +18,16 @@ $(function(){
       }
       console.log(input);
       var x = d3.scale.linear()
-          .domain([0, d3.max(input)])
-          .range([0, 420]);
+        .domain([d3.max(input), d3.min(input)])
+        .range([0, 420]);
 
       d3.select(".chart")
-        .selectAll("div")
+          .selectAll("div")
           .data(input)
         .enter().append("div")
           .style("width", function(d) { return x(d) + "px"; })
           .text(function(d) { return d; });
+
     },
     error: function (XMLHttpRequest, textStatus, errorThrown) {
       console.log('error test', errorThrown);
@@ -34,4 +35,71 @@ $(function(){
   });
 });
 
+// basic working bar chart
   // var data = [4, 8, 15, 16, 23, 42];
+  //   var x = d3.scale.linear()
+  //     .domain([0, d3.max(input)])
+  //     .range([0, 420]);
+
+  // d3.select(".chart")
+  //   .selectAll("div")
+  //     .data(input)
+  //   .enter().append("div")
+  //     .style("width", function(d) { return x(d) + "px"; })
+  //     .text(function(d) { return d; });
+
+// current attempt at vertical bar chart
+
+      // var margin = {top: 20, left: 10, right: 10, bottom: 20},
+      //   width = 700 - margin.left - margin.right,
+      //   height = 500 - margin.top - margin.bottom,
+      //   barPadding = 0.2;
+
+      // var y = d3.scale.linear()
+      //   .range([height, 0]);
+
+      // var x = d3.scale.ordinal()
+      //   .rangeRoundBands([0, 100], .2);
+
+      // var xAxis = d3.svg.axis()
+      //   .scale(x)
+      //   .orient("bottom")
+      //   .ticks(input.length);
+
+      // var yAxis = d3.svg.axis()
+      //   .scale(y)
+      //   .orient("left")
+
+      // var svg = d3.select('.chart').append('svg')
+      //               .attr("width", width + margin.left + margin.right)
+      //               .attr("height", height + margin.top + margin.bottom)
+      //             .append("g")
+      //               .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      // x.domain([0,input.length]);
+      // y.domain([0,d3.max(input)]);
+
+      // svg.append("g")
+      //     .attr("class", "x axis")
+      //     .attr("transform", "translate(0," + height + ")")
+      //     .call(xAxis);
+
+      // svg.append("g")
+      //     .attr("class", "y axis")
+      //     .call(yAxis)
+
+      // svg.selectAll(".bar")
+      //     .data(input)
+      //   .enter().append("rect")
+      //     .attr("class", "bar")
+      //     .attr("width", x.rangeBand())
+      //     .attr('x', function(d) { return x(d)})
+      //     .attr("y", function(d) { return y(d); })
+      //     .attr("height", function(d) { return height - y(d); })
+      //     .text(function(d) { return d; });
+
+
+
+
+
+
+     
